eda3ab8e7c492d91b92bef7bb8795b8cb317850e
Hazel Ann Espiso <hzlespiso@gmail.com>
2026-09-24 23:45:54 +0800

    Implement main class with device adapters

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args){
+
+        Laptop laptop = new Laptop();
+        PowerOutlet laptopOutlet = new LaptopAdapter(laptop);
+
+        Refrigerator refrigerator = new Refrigerator();
+        PowerOutlet refrigeratorOutlet = new RefrigeratorAdapter(refrigerator);
+
+        SmartphoneCharger smartphoneCharger = new SmartphoneCharger();
+        PowerOutlet smartphoneOutlet = new SmartphoneAdapter(smartphoneCharger);
+
+
+        System.out.println(laptopOutlet.plugIn());
+        System.out.println(refrigeratorOutlet.plugIn());
+        System.out.println(smartphoneOutlet.plugIn());
+    }
+}
